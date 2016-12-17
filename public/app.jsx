0333a06c2893b867ca2bdb9fb6724c943b3e52f4
Junaid Farooq <junaid@evercam.io>
2016-12-17 20:16:28 +0500

    Added multiple props.

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,23 @@
 var Greeter = React.createClass({
+  getDefaultProps : function() {
+    return {
+      name: "React@!",
+      msg: "hey whats up"
+    };
+  },
   render : function() {
+    var name = this.props.name;
+    var msg = this.props.msg;
     return (
       <div>
-        <h1> hello React! </h1>
+        <h1> hello {name}! </h1>
+        <p> {msg} </p>
       </div>
     );
   }
 });
 
 ReactDOM.render(
-  <Greeter />,
+  <Greeter/>,
   document.getElementById('app')
   );
